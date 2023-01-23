b60eaf3fa6f3c07a13cd79eeecd581a65d92baaf
Tahsin <tahsinhasanturab@gmail.com>
2023-01-23 23:38:09 +0600

    sign up page added

--- Library/Library.Infrastructure/Entities/User.cs
+++ Library/Library.Infrastructure/Entities/User.cs
@@ -4,9 +4,8 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string StudentId { get; set; }
         public string Email { get; set; }
-        public string Department { get; set; }
-        public int Batch { get; set; }
         public string Mobile { get; set; }
         public bool IsApproved { get; set; }
         public Role UserRole { get; set; }
