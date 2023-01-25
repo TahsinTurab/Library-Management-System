04009e7c8e755f4b8f3c735835625a0f17517287
Tahsin <tahsinhasanturab@gmail.com>
2023-01-25 06:56:17 +0600

    Remove unnecessary code

--- Library/Library.Infrastructure/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Library.Infrastructure.Entities
-{
-    public class ApplicationUser : IdentityUser<Guid>
-    {  
-        public string StudentId { get; set; }
-    }
-}
